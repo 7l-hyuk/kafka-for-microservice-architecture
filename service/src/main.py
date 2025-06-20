21bcdb4c2305358ea91f3e73ecbf499b2cae0ad8
GI-HYUK KWON <7lhyuk@gmail.com>
2025-06-20 15:45:25 +0900

    fix(service): service fail to Connect to kafka broker
    add loop to connect to kafka broker

--- service/src/main.py
+++ service/src/main.py
@@ -1,7 +1,8 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
-from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
+from aiokafka import AIOKafkaConsumer
+from aiokafka.errors import KafkaConnectionError
 import asyncio
 
 KAFKA_BROKER = "kafka:9092"
@@ -12,8 +13,14 @@ async def get_consumer():
         bootstrap_servers=KAFKA_BROKER,
         group_id="order-service-group"
     )
-    await consumer.start()
-    return consumer
+    for _ in range(10):
+        try:
+            await consumer.start()
+            return consumer
+        except:
+            await asyncio.sleep(5)
+    else:
+        raise RuntimeError("Kafka에 연결할 수 없습니다.")
 
 
 async def consume_orders():
